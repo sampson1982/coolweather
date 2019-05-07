29558cd6d0acecbf5198c78e1a8d2d673f277fa1
sampson1982 <270561599@qq.com>
2019-05-07 19:34:24 +0800

    增加

--- app/src/main/java/android/coolweather/com/coolweather/util/Utility.java
+++ app/src/main/java/android/coolweather/com/coolweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.coolweather.com.coolweather.db.City;
 import android.coolweather.com.coolweather.db.County;
 import android.coolweather.com.coolweather.db.Province;
 import android.text.TextUtils;
+import android.coolweather.com.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import com.google.gson.JsonIOException;
 
@@ -81,6 +83,20 @@ public class Utility {
             }
         }
         return  false;
+    }
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
